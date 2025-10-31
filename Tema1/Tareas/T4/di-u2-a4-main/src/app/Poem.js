c3a9f1b11a7b4ec9d532e71a3e3cac9beac67d33
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2025-10-31 10:48:59 +0100

    1

--- Tema1/Tareas/T4/di-u2-a4-main/src/app/Poem.js
+++ Tema1/Tareas/T4/di-u2-a4-main/src/app/Poem.js
@@ -1,21 +1,26 @@
 const poem = {
-    lines: [
-      'Escribo, borro y reescribo',
-      'Borro de nuevo, y luego',
-      'Florece una amapola.'
-    ]
-  };
-  
-  export default function Poem() {
-    return (
-      <article>
-        {poem.lines.map((line, index) =>
-          <p key={index}>
-            {line}
-            <hr />
-          </p>
-        )}
-      </article>
+  lines: [
+    'Escribo, borro y reescribo',
+    'Borro de nuevo, y luego',
+    'Florece una amapola.'
+  ]
+};
+export default function Poem() {
+  let output = [];
+  poem.lines.forEach((line, i) => {
+    output.push(
+      <hr key={i + '-separator'} />
     );
-  }
-  
+    output.push(
+      <p key={i + '-text'}>
+        {line}
+      </p>
+    );
+  });
+  output.shift();
+  return (
+    <article>
+      {output}
+    </article>
+  );
+}
--- Tema1/Tareas/T4/di-u2-a4-main/src/app/RecipeList2.js
+++ Tema1/Tareas/T4/di-u2-a4-main/src/app/RecipeList2.js
@@ -1,30 +1,26 @@
 import { recipes } from './recipes.js';
 
 export default function RecipeList2() {
-  const receta=recipes.map(recipe=>
-    <li key={recipe.id}>
-      <p>
-        <b>{recipe.name}:</b>
-      </p>
-    </li>
-  );
-  const Ingredientes=recipes.map(recipe=>
-    <ul key={recipe.id}>
-      {recipe.ingredients.map(ingredient=>
-        <li key={ingredient}>
-          {ingredient}
-        </li>
-      )}
-    </ul>
-  );
-  if (receta.key=Ingredientes.key) {
-        return (
+  return (
     <div>
       <h1>Recetas</h1>
-      <h2>{receta.name}</h2>
-      <ul>{Ingredientes.ingredients}</ul>      
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>
+            <p>
+              <b>{recipe.name}:</b>
+            </p>
+            <ul>
+              {recipe.ingredients.map(ingredient => (
+                <li key={ingredient}>
+                  {ingredient}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-      }
-  }
+}
 
