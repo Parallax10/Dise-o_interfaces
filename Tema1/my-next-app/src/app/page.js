4b4bc481402b42f8087b4ac417537057059703a6
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2025-11-07 14:43:50 +0100

    1

--- Tema1/my-next-app/src/app/page.js
+++ Tema1/my-next-app/src/app/page.js
@@ -1,16 +1,31 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "./Header";
-function MyButton(){
-  return<button>Hola</button>
+function Greeting({name}) {
+  return <h1>Hola, {name}</h1>;
+}
+function Card({titulo,descripcion,imagen}){
+  return(
+    <div>
+      <h1>{titulo}</h1>
+      <p>{descripcion}</p>
+      <img src={imagen}/>
+    </div>
+  );
+}
+function App(){
+  return(
+    <div>
+      <Card titulo="Titulo" descripcion="Descripcion" imagen='https://i.imgur.com/Mx7dA2Y.jpg'></Card>
+      <Card titulo="Titulo" descripcion="Descripcion" imagen='https://i.imgur.com/Mx7dA2Y.jpg'></Card>
+    </div>
+  );
 }
 
 export default function Home() {
   return (
-   <div>
-    <Header></Header>   
-    <p>Hola mundo</p>
-    <MyButton></MyButton>
-   </div>
+  <div>
+    <Greeting name="Paco"></Greeting>
+    <App></App>
+  </div>
   );
 }
