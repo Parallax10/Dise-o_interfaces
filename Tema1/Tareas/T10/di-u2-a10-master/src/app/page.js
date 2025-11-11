1a9e3a4752a060236fde417c8ebcc981c34c942e
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2025-11-11 13:51:28 +0100

    1

---
+++ Tema1/Tareas/T10/di-u2-a10-master/src/app/page.js
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+import styles from "./page.module.css";
+import Canvas from "./Canvas";
+import Scoreboard from "./Scoreboard";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Ejercicio 1</h1>
+      <Scoreboard />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Ejercicio 2</h1>
+      <Canvas />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Ejercicio 3</h1>
+      
+
+    </div>
+  );
+}
